d5e866e8fba599824265ad706505716bfe2eafa7
Kit Sunde <kitsunde@gmail.com>
2013-06-05 01:26:50 +0800

    Load lists through admin

--- mailwhimp/admin.py
+++ mailwhimp/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from mailwhimp.models import Campaign, List, Application
 
-admin.site.register(Application)
+
+class ApplicationAdmin(admin.ModelAdmin):
+    actions = ['load_lists']
+
+    def load_lists(self, request, queryset):
+        for application in queryset.all():
+            application.load_lists()
+
+
+admin.site.register(Application, ApplicationAdmin)
 admin.site.register(List)
 admin.site.register(Campaign)
